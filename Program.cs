d18264f3fee73287307d71d1a802a192ca0b56c6
LucasCatanelli <135190473+LucasCatanelli@users.noreply.github.com>
2024-02-22 21:36:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondClassCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grausFahrenheit = 0, grausCelcius = 0;
+
+            Console.WriteLine("Digite o valor em graus Fahrenheit: ");
+            grausFahrenheit = Convert.ToInt16(Console.ReadLine());
+            grausCelcius = (grausFahrenheit - 32.0) * (5.0 / 9.0);
+
+            Console.WriteLine("O valor digitado em graus Celsius e de " + grausCelcius.ToString(".0"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
